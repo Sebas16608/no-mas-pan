9ec7e7b3f573896ec15f5cb45bf5afad20321ffb
Sebas16608 <asebasrr444@gmail.com>
2026-03-27 11:07:08 -0600

    chore: Creating the DB config

---
+++ backend/src/config/database.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+import path from "node:path";
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite3"),
+    logging: false
+});
+
+export default sequelize;

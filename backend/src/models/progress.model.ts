d359208fe1f24385373f7a36c8b2c5c16bcff3d6
Sebas16608 <asebasrr444@gmail.com>
2026-03-29 09:42:50 -0600

    chore: Creating the progress model

---
+++ backend/src/models/progress.model.ts
@@ -0,0 +1,60 @@
+import sequelize from "../config/database";
+import User from "./user.model";
+import { DataTypes, Model } from "sequelize";
+
+class Progress extends Model {
+    declare id?: number;
+    declare userId: number;
+    declare date: Date;
+    declare weight: number | null;
+    declare body_fat: number | null;
+}
+
+Progress.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+    },
+    date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    weight: {
+        type: DataTypes.DECIMAL(5, 2),
+        allowNull: true,
+        validate: {
+            min: 0
+        }
+    },
+    body_fat: {
+        type: DataTypes.DECIMAL(5, 2),
+        allowNull: true,
+        validate: {
+            min: 0,
+            max: 100
+        }
+    },
+},{
+    sequelize,
+    modelName: "Progress",
+    freezeTableName: true,
+    indexes: [
+        {
+            unique: true,
+            fields: ["userId", "date"]
+        }
+    ]
+})
+
+export default Progress;

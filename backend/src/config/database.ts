8d4eb8d3f8d7f32099335b012ae93e327688c0b5
Sebas16608 <asebasrr444@gmail.com>
2026-03-30 13:43:54 -0600

    fix: fixing the db config

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: path.join(__dirname, "db.sqlite3"),
-    logging: false
+    logging: false,
 });
 
 export default sequelize;

d999bcf6ecf00e378aec6b678554f40d37430ce0
Sebas16608 <asebasrr444@gmail.com>
2026-04-10 16:05:15 -0600

    chore: db synchronized

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import app from "./app";
 import "./models/association";
+import sequelize from "./config/database";
 
 const port = 3000;
 
 async function main() {
     try {
+        await sequelize.authenticate();
+        console.log("DB establecida");
+
+        await sequelize.sync({ alter: true });
+        console.log("DB sincronizada");
+
         app.listen(port, () => {
             console.log(`Servidor corriendo en http://localhost:${port}`)
         })

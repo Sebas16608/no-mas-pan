4c572c11fcad26f25cd6f31c703acb3fe1fad14c
Sebas16608 <asebasrr444@gmail.com>
2026-04-13 22:14:58 -0600

    chore: change the schema to the minimal and power schema

--- backend/src/food/food.schema.ts
+++ backend/src/food/food.schema.ts
@@ -1,28 +1,9 @@
-import z, { minLength } from "zod";
-import { da } from "zod/locales";
+import z from "zod";
 
 export const FoodSchema = z.object({
-    body: z.object({
-        name: z.string().min(5, "Minimo 5 caracteres"),
-        calories: z.number().positive().min(3, "Minimo 3 caracteres"),
-        protein: z.number().positive().min(3, "Minimo 3 caracteres"),
-        carbs:  z.number().positive().min(3, "Minimo 3 caracteres"),
-        fats: z.number().positive().min(3, "Minimo 3 caracteres"),
-    })
+    name: z.string().min(5, "Minimo 5 caracteres"),
+    calories: z.number().positive().min(3),
+    protein: z.number().positive().min(3).optional(),
+    carbs: z.number().positive().min(3).optional(),
+    fats: z.number().positive().min(3).optional(),
 });
-
-
-export const data = {
-    name: "comida",
-    calories: 150,
-    protein: 150,
-    carbs: 150,
-    fats: 150,
-}
-
-try {
-    const validData = FoodSchema.parse(data);
-    console.log(validData);
-} catch (error) {
-    console.error(error);
-}

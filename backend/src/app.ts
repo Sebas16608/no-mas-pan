9fa25555045c50a24fc57c44709b3d8a7d691eb7
Sebas16608 <asebasrr444@gmail.com>
2026-04-14 19:55:44 -0600

    chore: adding the food route in the routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import FoodRouter from "./food/food.router";
 
 const app = express();
 
@@ -13,7 +14,10 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome",
+        endpoints: "/food",
     })
 })
+
+app.use("/food", FoodRouter);
 
 export default app;
